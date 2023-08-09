e9fe7c2e479a5824a06120e9178c26e06589f4e1
VisiDK <43777517+VisiDK@users.noreply.github.com>
2023-08-09 22:32:08 +0200

    Upload of Invite link
    A front-end invite system for csgo leagues

---
+++ index.php
@@ -0,0 +1,80 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/3a1b0955d6.js" crossorigin="anonymous"></script> 
+  </head>
+  <body style="background-image: url('back1.png');">
+
+  <div class="team-invite-box">
+    <div class="custom-select">
+      <h1> League Name </h1>
+               <div class="container">
+                  <div class="profile-picture">
+                     <div class="profile-img">
+                       <img src="king-bear-Transparent_233.png" style="width: 150px; height: 150px;">
+                     </div>
+                  </div>
+               </div>
+    
+      <p>Vælg hold</p>
+      <select class="dropdown" id="tableselector">
+        <option value="team-1">team 1</option>
+        <option value="team-2">team 2</option>
+        <option value="team-3">team 3</option>
+        <option value="team-4">team 5</option>
+        <option value="team-5">team 6</option>
+        <option value="team-6">team 7</option>
+        <option value="team-7">team 8</option>
+     </select>
+    </div>
+
+
+  </br>
+  </br>
+<div class="container">    
+    <div style="font-size: 1rem;">
+      <hr style="color: white;">
+
+      <div class="d-flex align-items-center text-white margin-icon icon icon_hover_text"><i class="fa-regular fa-calendar fa-fw" title="Back"></i></span><span class="ms-2">League periode</span> </div>
+      <div class="popup_element">test text.</div>
+      
+      <div class="d-flex align-items-center text-white margin-icon icon icon_hover_text"><i class="fa-regular fa-calendar fa-fw" title="Back"></i></span><span class="ms-2">Signup periode</span> </div>
+      <div class="popup_element">test text.</div>
+      
+      <div class="d-flex align-items-center text-white margin-icon icon icon_hover_text"><i class="fa-solid fa-trophy fa-fw" title="Back"></i></span><span class="ms-2">League format</span></div>
+      <div class="popup_element">test text.</div>
+      
+      <div class="d-flex align-items-center text-white margin-icon icon icon_hover_text"><i class="fa-solid fa-hand-fist fa-fw" title="Back"></i></span><span class="ms-2">Match format</span></div>
+      <div class="popup_element">test text.</div>
+      
+      <div class="d-flex align-items-center text-white margin-icon icon icon_hover_text"><i class="fa-solid fa-bolt fa-fw" title="Back"></i></span><span class="ms-2">ELO</span></div>
+      <div class="popup_element">test text.</div>
+      
+      <div class="d-flex align-items-center text-white margin-icon icon icon_hover_text"><i class="fa-regular fa-money-bill-1 fa-fw" title="Back"></i></span><span class="ms-2">Prize poll</span></div>
+      <div class="popup_element">test text.</div>
+      
+      <div class="d-flex align-items-center text-white margin-icon icon icon_hover_text"><i class="fa-solid fa-dollar-sign fa-fw" title="Back"></i></span><span class="ms-2">Entry fee</span></div>
+      <div class="popup_element">test text.</div>
+  <hr style="color: white;">
+
+  </br>
+  </br>
+
+  <span class="right" style="border: 1px solid red; border-radius: 0.25em; padding: 0.5em; background-color: red;"><i class="fa-brands fa-discord fa-fw right" title="Back"></i></span>
+  <span class="right" style="border: 1px solid red; border-radius: 0.25em; padding: 0.5em; background-color: red;"><i class="fa-solid fa-book fa-fw right" title="Back"></i></span>
+  <a class="left" href="#"> <img src="king-bear-Transparent_233.png" style="width: 55px; height: 55px;"> </a>
+
+</div>
+</div>
+
+  </div>
+</div>
+  </body>
+
+</html>
+
+
+
+<img onclick="">Click me</butt
